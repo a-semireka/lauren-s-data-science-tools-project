0dc8a8a6d5a6aaa4f1c8e7e95a919e74f6233b73
a-semireka <147056603+a-semireka@users.noreply.github.com>
2023-10-24 01:24:26 +0000

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# # summary of data science tools and ecosystem
+
+# # some of the popular languages that Data Scientists use are:
+# 1. python
+# 2. R
+# 3. SQL( structured Query Language
+# 4. Julia
+# 5. Scala
+# 6. SAS(Statistical Analysis System)
+
+# # some commomly used data science libraries are:
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+# 4. Scikit-learn
+# 5. Tensorflow
+# 6. Scipy
+# 7. PyTorch
+# 8. Seaborn
+# 9.StatsModels
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | VS Code            |
+
+# ### Below are a few examples of arithmetic expressions in python.
+
+# In[12]:
+
+
+# This is a simple arithmetic expression to multiply and add integers
+results = (3*4) + 5
+
+
+# In[13]:
+
+
+# This will convert 300 inutes to hours by dividing by 60.
+hours = 200/60
+print(hours)
+
+
+# ** Objectives**
+# - introduction to data science libraries
+# - describe data science tools
+# - list popular data science languages
+
+# # AUTHOR
+# Amo Lawrencia Semireka
